ec465378b73873656b963bf65a051b586107cb89
Ryan Walters <RyanWaltersDev@gmail.com>
2021-05-26 15:21:22 -0400

    +add user.py +add to glossary/fave

--- favorite_languages.py
+++ favorite_languages.py
@@ -20,4 +20,24 @@ print(f"Sarah's favorite language is {sarah_language}!")
 print(f"Edward's favorite language is {edward_language}!")
 print(f"Phil's favorite language is {phil_language}!")
 
+# Use for loop to complete same task
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}")
+
+# Use for loop for keys only
+for name in favorite_languages.keys():
+    print(f"{name.title()}")
+
+# Print message for keys in a list
+friends = ['sarah', 'phil']
+for name in favorite_languages.keys():
+    print(f"{name.title()}")
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\tOh hey {name.title()}! I see you love {language}!")
+
+# Check for key in list
+if 'bob' not in favorite_languages.keys():
+    print("Bob, please take our poll!")
+
 # END OF PROGRAM
