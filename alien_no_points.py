ac99c063fe32514d06fd60088be6883763529ee0
Ryan Walters <RyanWaltersDev@gmail.com>
2021-05-25 23:34:14 -0400

    Ch6 new

---
+++ alien_no_points.py
@@ -0,0 +1,9 @@
+# RyanWaltersDev May 25 2021 -- Key-Value error, avoid with get()
+
+# Define dictionary
+alien_0 = {'color': 'green', 'speed': 'slow'}
+
+point_value = alien_0.get('points', 'No point value assigned.')
+print(point_value)
+
+# END OF PROGRAM
---
+++ person.py
@@ -0,0 +1,33 @@
+# RyanWaltersDev May 25 2021 -- TIY 6-1
+
+# Define dictionary
+brooke_yost = {
+    'first_name': 'brooke', 
+    'last_name': 'yost', 
+    'age': 23, 
+    'location': 'milledgeville',
+    'favorite color': 'cranberry',
+    'favorite disney movie': 'aladdin',
+    'booty': 'unquantifiable',
+    }
+
+# Assign variables for each key-value pair
+first_name = brooke_yost['first_name'].title()
+last_name = brooke_yost['last_name'].title()
+age = brooke_yost['age']
+location = brooke_yost['location'].title()
+favorite_color = brooke_yost['favorite color'].title()
+favorite_disney_movie = brooke_yost['favorite disney movie'].title()
+booty_variable = brooke_yost['booty'].title()
+
+# Print each value
+print(f"Her first name is {first_name}.")
+print(f"Her last name is {last_name}.")
+print(f"She is {age} years old.")
+print(f"She lives in {location}.")
+print(f"Her favorite color is {favorite_color}.")
+print(f"Her favorite Disney movie is {favorite_disney_movie}.")
+print(f"Her booty is {booty_variable}.")
+print("She made me say the last one.")
+
+# END OF PROGRAM
